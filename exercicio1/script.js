f8f8496725c97cd5171c94e146cd49a5ac512333
tcariri <thais.cariri2016@gmail.com>
2022-10-06 15:07:47 -0400

    exercicio finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,15 @@
-const array = [
+const gols = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
     [5, 5, 12, 16, 4],
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for ( let i = 0; i<gols.length;i++){
+    console.log(` jogador 1:${gols[0]} 
+                  \n jogador 2:${gols[1]}\n 
+                  \n jogador 3:${gols[2]}\n 
+                  \n jogador 4:${gols[3]}\n
+                  \n jogador 5:${gols[4]}\n`)
+}
---
+++ exercicio2/script.js
@@ -0,0 +1,7 @@
+const num = Number(prompt("insira um número de 1 a 10"))
+const array =[1,2,3,4,5,6,7,8,9,10]
+
+for (i in array){
+    console.log(`${num} x ${array[i]} = ${num*array[i]}`)
+}
+
